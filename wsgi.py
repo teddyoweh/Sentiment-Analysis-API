8bcfac806c6541e36acb10fdfccaadec8015e1e3
Teddy Oweh <teddyoweh@gmail.com>
2022-05-30 04:11:56 -0500

    Migrations

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,6 @@ from app import main
 # launch
 #----------------------------------------
 
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	main.run(host='0.0.0.0', port=port)
  
+if __name__ == "__main__":
+      main.run()
