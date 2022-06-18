fe8bf7e6f98e6c4fa230964d23176f4fe27b4e9e
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-18 04:33:37 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+
+  
+# -*- coding=utf-8 -*-
+# Name: teddy oweh
+# Email: teddyoweh@teddyoweh.com
+# Message: Feel Free To Contact Me on Enquires or Questions.
+import os
+from app import main
+
+
+#----------------------------------------
+# launch
+#----------------------------------------
+
+ 
+if __name__ == "__main__": 
+      main.run()
